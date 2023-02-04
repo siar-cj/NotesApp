1cd5007e003663bf21ed71bc7a620a576d40ba26
LUGIA <79367636+LSZTM@users.noreply.github.com>
2023-02-05 05:16:32 +0530

    Update saving.py

--- saving.py
+++ saving.py
@@ -1,67 +1,96 @@
 import os
 def check_access():
    #check if config file directory and notes directory exists, if it doesnt create respectively and return true
-  if configdir_exists() and Notesdir_exists() and check_notes_exists:
-    return True
-  else:
-    create_configdir()
-    create_notesdir()
-    return True
-       
+    try:
+        if configdir_exists() and Notesdir_exists() and check_notes_exists():
+            return True
+        else:
+            create_configdir()
+            create_notesdir()
+            return True
+    except OSError as f:
+        print(f"{f}: occurred try again")   
 
 def saving_dir():
-  if not os.path.exists(f'{os.getcwd()}'+'Saving'):
-    os.mkdir(os.path.join(f'{os.getcwd()}','Saving'))
-  return True  
-
+    try:
+        if not os.path.exists(os.path.join(f'{os.getcwd()}','Saving')):
+            os.mkdir(os.path.join(f'{os.getcwd()}','Saving'))
+            return True  
+    except OSError as f:
+         print(f"{f}: occurred try again")   
 
 def configdir_exists():
-     if os.path.exists(return_configdir()):
-      return True
+    try:
+        if os.path.exists(return_configdir()):
+            return True
+    except OSError as f:
+         print(f"{f}: occurred try again")   
 
 
 def Notesdir_exists():
-  if os.path.exists(return_configdir()):
-      return True
-  else:
-    return False    
+    try:
+
+        if os.path.exists(return_configdir()):
+            return True
+        else:
+            return False    
      #check if notes dir exists
+    except OSError as f:
+         print(f"{f}: occurred try again")   
 
 
 def create_configdir():
-  os.mkdir(os.path.join(f'{os.getcwd()}'+'SAVING','CONFIGURATION'))
+    try:
+        os.mkdir(os.path.join(f'{os.getcwd()}','SAVING'))
+        os.mkdir(os.path.join(f'{os.getcwd()}','SAVING','CONFIGURATION'))
     #make config directory
+    except OSError as f:
+         print(f"{f}: occurred try again")   
 
 
 def create_notesdir():
-  os.mkdir(os.path.join(f'{os.getcwd()}','SAVING','NOTES'))
+    try:
+        os.mkdir(os.path.join(f'{os.getcwd()}','SAVING','NOTES'))
     #make notes config dir
+    except OSError as f:
+         print(f"{f}: occurred try again")   
 
 
 def check_notes_exists():
+    try:
   #returns true if both note file and respective json file exists
-  configdir = []
-  notesdir = []
-  for  files in os.walk(return_notesdir()):
-    for i in files:
-      configdir.append(os.path.splitext(os.path.join(f'{return_notesdir()}',i))[0])
-  
-  for files in os.walk(return_configdir()):
-    for i in files:
-      notesdir.append(os.path.splitext(os.path.join(f'{return_configdir()}',i))[0])    
-  if set(configdir)== set(notesdir):
-    return True
+        configdir = []
+        notesdir = []
+        for  files in os.walk(return_notesdir()):
+            for i in files:
+                configdir.append(os.path.splitext(os.path.join(f'{return_notesdir()}',i))[0])
+        
+        for files in os.walk(return_configdir()):
+            for i in files:
+                notesdir.append(os.path.splitext(os.path.join(f'{return_configdir()}',i))[0])    
+        if set(configdir)== set(notesdir):
+            return True
+    except OSError as f:
+         print(f"{f}: occurred try again")   
 
   
 
     
 
 def return_configdir():
-  return os.path.join(f'{os.getcwd()}','SAVING','CONFIGURATION')
-    
+    try:
+        return os.path.join(f'{os.getcwd()}','SAVING','CONFIGURATION')
+    except OSError as f:
+         print(f"{f}: occurred try again")   
+        
 
 def return_notesdir():
+    try:
   #returns notes directory
-  return os.path.join(f'{os.getcwd()}','SAVING','NOTES')
-    
+        return os.path.join(f'{os.getcwd()}','SAVING','NOTES')
+    except OSError as f:
+         print(f"{f}: occurred try again")   
+check_access()    
+  
+
 
