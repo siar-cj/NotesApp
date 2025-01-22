5d21e5d4f78c0957a700906bd524f1c6731e16d6
Siva CS Javvadhi <118528602+siar-cj@users.noreply.github.com>
2025-01-22 11:56:38 +0530

    Update NotesApp.py

--- NotesApp.py
+++ NotesApp.py
@@ -1,1 +1,10 @@
-print("Work In Progress")
+import _background
+import _reminder_window
+import logger
+import CRUD
+import crud_api
+import reminder
+import saving
+
+def main():
+    print("work in progress")
